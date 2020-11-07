5a94b7d67f580d8961a96a71c8edcda92e6d0a0d
cbwestaway <charleswestaway@gmail.com>
2020-11-06 22:52:48 -0500

    More Distance Analysis
    Added plots/analysis for the marker to control line and control line to test line distances.

--- homography_analysis.m
+++ homography_analysis.m
@@ -9,8 +9,10 @@ N = 4;
 numFiles = length(imPaths);
 % define arrays for storing height data
 markerHeights = zeros(1, numFiles);
-controlLineHeights = zeros(1, numFiles);
-testLineHeights = zeros(1, numFiles);
+mcDists = zeros(1, numFiles);
+clHeights = zeros(1, numFiles);
+ctDists = zeros(1, numFiles);
+tlHeights = zeros(1, numFiles);
 for p=1:numFiles
     I = imread(imPaths{p});
     % Height and distance counts
@@ -56,21 +58,23 @@ for p=1:numFiles
     fprintf('Marker H=%.1f \n', mHeight);
     markerHeights(p) = mHeight;
     % Marker to Control Dist
-    mCDist = cStart - mEnd;
+    mcDist = (cStart - mEnd) / mHeight;
+    mcDists(p) = mcDist;
     fprintf('Marker Control H=%i \n', mCDist);
     % Control Height
-    cHeight = cEnd - cStart;
+    cHeight = (cEnd - cStart) / mHeight;
     fprintf('Control Line H =%i \n', cHeight);
     % Store the height proportional to the marker
-    controlLineHeights(p) = cHeight / mHeight;
+    clHeights(p) = cHeight;
     % Control Test Dist
-    cTDist = tStart - cEnd;
+    ctDist = (tStart - cEnd) / mHeight;
+    ctDists(p) = ctDist;
     fprintf('Control Test H=%i \n', cTDist);
     % Test Line Height
-    tHeight = tEnd - tStart;
+    tHeight = (tEnd - tStart) / mHeight;
     fprintf('Test Line H =%i \n', tHeight);
     % Store the height proportional to the marker
-    testLineHeights(p) = tHeight / mHeight;
+    tlHeights(p) = tHeight;
 end
 figure;
 histogram('Categories', imPaths , 'BinCounts', markerHeights);
@@ -78,24 +82,32 @@ histogram('Categories', imPaths , 'BinCounts', markerHeights);
 mu =  num2str(round(mean(markerHeights), 2));
 sig = num2str(round(std(markerHeights), 2));
 title(['Marker Heights (mu =', mu, ', sigma = ', sig, ')']);
-% Probability Distribution
-pdMarker = fitdist(markerHeights', 'Normal');
-ci = paramci(pdMarker);
+
 figure;
-histogram('Categories', imPaths , 'BinCounts', controlLineHeights);
+histogram('Categories', imPaths , 'BinCounts', mcDists);
 % Get mean and std for hist
-mu =  num2str(round(mean(controlLineHeights), 2));
-sig = num2str(round(std(controlLineHeights), 2));
+mu =  num2str(round(mean(mcDists), 2));
+sig = num2str(round(std(mcDists), 2));
+title(['Marker Control Distances wrt Marker (mu =', mu, ', sigma = ', sig, ')']);
+
+figure;
+histogram('Categories', imPaths , 'BinCounts', clHeights);
+% Get mean and std for hist
+mu =  num2str(round(mean(clHeights), 2));
+sig = num2str(round(std(clHeights), 2));
 title(['Control Line Heights wrt Marker (mu =', mu, ', sigma = ', sig, ')']);
-% Probability Distribution
-pdControl = fitdist(controlLineHeights', 'Normal');
-ci = paramci(pdControl);
+
 figure;
-histogram('Categories', imPaths , 'BinCounts', testLineHeights);
+histogram('Categories', imPaths , 'BinCounts', ctDists);
 % Get mean and std for hist
-mu =  num2str(round(mean(testLineHeights), 2));
-sig = num2str(round(std(testLineHeights), 2));
+mu =  num2str(round(mean(ctDists), 2));
+sig = num2str(round(std(ctDists), 2));
+title(['Control Test Distances wrt Marker (mu =', mu, ', sigma = ', sig, ')']);
+
+figure;
+histogram('Categories', imPaths , 'BinCounts', tlHeights);
+% Get mean and std for hist
+mu =  num2str(round(mean(tlHeights), 2));
+sig = num2str(round(std(tlHeights), 2));
 title(['Test Line Heights wrt Marker (mu =', mu, ', sigma = ', sig, ')']);
-% Probability Distribution
-pdTest = fitdist(testLineHeights', 'Normal');
-ci = paramci(pdTest);
+
