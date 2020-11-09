0d9e490d807c97a597bb4bd7dcdf991578d2679b
cbwestaway <charleswestaway@gmail.com>
2020-11-08 19:52:15 -0500

    getMarker Function
    - Moved the logic to get the marker's starting and ending positions to its own file.

---
+++ getMarker.m
@@ -0,0 +1,42 @@
+function [mStart, mEnd, mHeight] = getMarker(I)
+    % Marker height
+    mStart = 0;
+    mEnd = 0;
+    mHeight = 0;
+    wid = width(I);
+    for i=1:length(I)
+        % Identify starting and ending rows of the marker
+        if mStart == 0 && isMarker(i, I, wid)
+            mStart = i;
+        elseif mStart > 0 && mEnd == 0 && isMarker(i, I, wid) == false
+            mEnd = i;
+            mHeight = i - mStart;
+            break;
+        end
+    end
+end
+
+
+%{
+    Detects the black marker above the strip.
+    Works well for now but will likely need to make
+    a more sophisticated detector.
+%}
+function isMarker = isMarker(i, I, wid)
+    bThresh = 0.4; 
+    passThresh = 0.8;
+    passes = 0;
+    for j=1:wid
+        r = I(i, j, 1);
+        g = I(i, j, 2);
+        b = I(i, j, 3);
+        if r < bThresh && g < bThresh && b < bThresh
+            passes = passes + 1;
+        end
+    end
+    if passes / wid > passThresh
+        isMarker = true;
+    else
+        isMarker = false;
+    end
+end
