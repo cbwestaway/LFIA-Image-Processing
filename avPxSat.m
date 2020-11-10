bcf152495fcea97413e37d27b12b05e02d0df065
cbwestaway <charleswestaway@gmail.com>
2020-11-09 23:12:40 -0500

    Updated Pixel Analysis Functions
    - Modified the pixel analysis functions to include all columns of pixels in the image as zoomed and properly cropped images will be used.

--- avPxSat.m
+++ avPxSat.m
@@ -1,10 +1,6 @@
 function av = avPxSat(I, startRow, endRow)
     wid = width(I);
-    startCol = 25;
-    endCol = wid - 25;
-    % Equilizing the histogram should sharpen the test line
-    I = histeq(I);
     hsvI = rgb2hsv(I);
-    px = hsvI(startRow:endRow, startCol:endCol, 2);
+    px = hsvI(startRow:endRow, :, 2);
     av = mean(px ./ ((endRow - startRow) * wid));
 end
--- avRedPx.m
+++ avRedPx.m
@@ -1,9 +1,5 @@
 function av = avRedPx(I, startRow, endRow)
     wid = width(I);
-    startCol = 25;
-    endCol = wid - 25;
-    % Equilizing the histogram should sharpen the test line
-    I = histeq(I);
-    px = I(startRow:endRow, startCol:endCol, 1);
+    px = I(startRow:endRow, :, 1);
     av = mean(px ./ ((endRow - startRow) * wid));
 end
