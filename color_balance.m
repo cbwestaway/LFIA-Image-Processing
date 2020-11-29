fd9f4314e92eac954597e998b2f4a5b186b20cf1
cbwestaway <charleswestaway@gmail.com>
2020-11-29 13:35:11 -0500

    Updated get_mean_strip_color
    - Updated the "get mean strip color" function to return the whitest pixel as a reference for color balancing.
    - This appears to produce a more consistent result.

--- color_balance.m
+++ color_balance.m
@@ -1,9 +1,10 @@
 function balanced = color_balance(img)
     [r, g, b] = get_mean_strip_color(img);
+    base_white = 1;
     % define scaling so that the strip appears to be white
-    r_s = 1 / r;
-    g_s = 1 / g;
-    b_s = 1 / b;
+    r_s = base_white / r;
+    g_s = base_white / g;
+    b_s = base_white / b;
     % multiply the image by the scaling to color balance
     balanced = img;
     balanced(:, :, 1) = balanced(:, :, 1) .* r_s;
--- get_mean_strip_color.m
+++ get_mean_strip_color.m
@@ -1,14 +1,11 @@
 function [r, g, b] = get_mean_strip_color(img)
-    len = length(img);
-    wid = width(img);
-    img = histeq(img);
-    % get the last bit of the image
-    strip_end = img(len-50:len, :);
-    r = mean(strip_end(1), 'all');
-    disp(r);
-    g = mean(strip_end(2), 'all');
-    disp(g);
-    b = mean(strip_end(3), 'all');
-    disp(b);
+    % Find the brightest pixel in the image
+    S = sum(img, 3);
+    [~,idx] = max(S(:));
+    [i, j] = ind2sub(size(S),idx);
+    % Return the rgb values of the brightest pixel
+    r = img(i, j, 1);
+    g = img(i, j, 2);
+    b = img(i, j, 3);
 end
 
